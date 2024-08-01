e3deb230c8596f4415b609d3172b70e6137bf985
Gorciu <gorciuyt@gmail.com>
2024-08-01 11:20:47 +0200

    Create custom Discord app

---
+++ src/main.js
@@ -0,0 +1,36 @@
+/**
+ * VexaCord - custom Discord app with a great modification built-in!
+ * (C) 2024 VexaCord
+ * 
+ * When you contribute, we and you have copyright to your changes.
+ */
+
+const {app, BrowserWindow} = require('electron');
+
+/**
+ * A function that runs VexaCord
+ */
+function runVexaCord() {
+    let bw = new BrowserWindow({
+        minHeight: 800,
+        minWidth: 1000,
+
+        webPreferences: {
+            nodeIntegration: false,
+            nodeIntegrationInSubFrames: false,
+            nodeIntegrationInWorker: false,
+            contextIsolation: true
+        }
+    });
+
+    bw.loadURL('https://canary.discord.com/app', {
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3'
+    });
+}
+
+app.whenReady().then(runVexaCord);
+app.on('activate', runVexaCord);
+
+app.on('window-all-closed', () => {
+    app.quit();
+})
